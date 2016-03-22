763bc234c3f8fd9d30a2ebadbd7bfcf3f40e628b
Sam Dean <deanWombourne@gmail.com>
2016-03-22 22:07:43 +0000

    Slicker if let statement

--- Pod/Classes/NSURLSession/SuccessSessionMock.swift
+++ Pod/Classes/NSURLSession/SuccessSessionMock.swift
@@ -11,29 +11,29 @@ import Foundation
 private let mult = Double(NSEC_PER_SEC)
 
 class SuccessSessionMock : SessionMock {
-    
+
     private let requestMatcher: RequestMatcher
     private let response: MockResponseHandler
     private let delay: Double
-    
+
     init(matching requestMatcher: RequestMatcher, response: MockResponseHandler, delay: Double) {
         self.requestMatcher = requestMatcher
         self.response = response
         self.delay = delay
     }
-    
+
     func matchesRequest(request: NSURLRequest) -> Bool {
         switch(self.requestMatcher.matches(request)) {
         case .Matches: return true
         case .NoMatch: return false
         }
     }
-    
+
     func consumeRequest(request: NSURLRequest, session: NSURLSession) throws -> NSURLSessionDataTask {
 
         switch (self.requestMatcher.matches(request)) {
 
-        // If this isn't for us, we shouldn't have been called, throw and let
+            // If this isn't for us, we shouldn't have been called, throw and let
         // everyone know!
         case .NoMatch:
             throw SessionMockError.InvalidRequest(request: request)
@@ -42,67 +42,65 @@ class SuccessSessionMock : SessionMock {
         case .Matches(let extractions):
             let task = MockSessionDataTask() { task in
                 task._state = .Running
-                
+
                 let timeDelta = 0.02
                 var time = self.delay
 
                 let response = self.response(url: request.URL!, extractions: extractions)
-                
-                if let delegate = session.delegate as? NSURLSessionDataDelegate {
-                    
-                    if let body = response.body {
-                        
-                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(mult * time)), dispatch_get_main_queue()) {
-                            let response = NSHTTPURLResponse(URL: request.URL!, statusCode: response.statusCode, HTTPVersion: "HTTP/1.1", headerFields: response.headers)!
-                            task.response = response
-                            delegate.URLSession?(session, dataTask: task, didReceiveResponse: response) { _ in }
-                        }
-                        
-                        time += timeDelta
-                            
-                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(mult * time)), dispatch_get_main_queue()) {
-                            delegate.URLSession?(session, dataTask: task, didReceiveData: body)
-                        }
-                        
-                        time += timeDelta
+
+                if let delegate = session.delegate as? NSURLSessionDataDelegate,
+                    let body = response.body {
+
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(mult * time)), dispatch_get_main_queue()) {
+                        let response = NSHTTPURLResponse(URL: request.URL!, statusCode: response.statusCode, HTTPVersion: "HTTP/1.1", headerFields: response.headers)!
+                        task.response = response
+                        delegate.URLSession?(session, dataTask: task, didReceiveResponse: response) { _ in }
                     }
+
+                    time += timeDelta
+
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(mult * time)), dispatch_get_main_queue()) {
+                        delegate.URLSession?(session, dataTask: task, didReceiveData: body)
+                    }
+
+                    time += timeDelta
                 }
-                
+
                 if let delegate = session.delegate as? NSURLSessionTaskDelegate {
-                    
+
                     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(mult * time)), dispatch_get_main_queue()) {
                         delegate.URLSession?(session, task: task, didCompleteWithError: nil)
                         task._state = .Completed
                     }
                 }
             }
-            
+
             task._originalRequest = request
-            
+
             return task
         }
     }
-    
+
 }
 
 class SingleSuccessSessionMock : SuccessSessionMock {
-    
+
     var canRun = true
-    
+
     override func matchesRequest(request: NSURLRequest) -> Bool {
         return canRun && super.matchesRequest(request)
     }
-    
+
     override func consumeRequest(request: NSURLRequest, session: NSURLSession) throws -> NSURLSessionDataTask {
         guard self.matchesRequest(request) else { throw SessionMockError.InvalidRequest(request: request) }
-        
+
         guard canRun else { throw SessionMockError.HasAlreadyRun }
-        
+
         let task = try super.consumeRequest(request, session: session)
-        
+
         canRun = false
-        
+
         return task
     }
-    
+
 }
